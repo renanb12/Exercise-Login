71e897d439bbc0f85fba194c325b92b206bf79cd
05632593029 <05632593029@senacrs.edu.br>
2024-04-26 21:40:46 -0300

    First commit

---
+++ Login/src/main/java/br/com/renanb12/login/Login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.renanb12.login;
+
+/**
+ *
+ * @author 05632593029
+ */
+public class Login {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
